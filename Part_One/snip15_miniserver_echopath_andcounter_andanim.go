d8129402003329c7c67fa9af2ff02a26ebb7e4ad
Ezio <assassinste@gmail.com>
2019-08-21 00:14:19 +0800

    Refactored: decouple anim function

--- Part_One/snip15_miniserver_echopath_andcounter_andanim.go
+++ Part_One/snip15_miniserver_echopath_andcounter_andanim.go
@@ -3,23 +3,14 @@ package main
 
 import (
 	"fmt"
-	"image"
-	"image/color"
-	"image/gif"
-	"io"
+	"github.com/code_go_book_go_programming_language/Part_One/lib"
 	"log"
-	"math"
-	"math/rand"
 	"net/http"
 	"sync"
 )
 
 var mu sync.Mutex
 var count int
-var palette = []color.Color{color.White, color.Black}
-
-const whiteIndex = 0
-const blackIndex = 1
 
 func main() {
 	/*
@@ -37,7 +28,7 @@ func main() {
 
 	// Optional
 	http.HandleFunc("/pattern", func(w http.ResponseWriter, r *http.Request) {
-		lissajous(w)
+		Part_One.Lissajous(w)
 	})
 
 	log.Fatal(http.ListenAndServe("localhost:7777", nil))
@@ -56,42 +47,4 @@ func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	_, _ = fmt.Fprintf(w, "Count %d\n", count)
 	mu.Unlock()
-}
-
-func lissajous(out io.Writer) {
-	/*
-		This function is optional, since we're just using it for demonstration
-		that is the `lissajous` function can also be written to the HTTP client.
-	*/
-	const (
-		cycles  = 5
-		res     = 0.001
-		size    = 100
-		nframes = 64
-		delay   = 8
-	)
-	freq := rand.Float64() * 3.0
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0
-
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(
-				size+int(x*size+0.5),
-				size+int(y*size+0.5),
-				blackIndex,
-			)
-		}
-		phase += 0.1
-
-		anim.Image = append(anim.Image, img)
-		anim.Delay = append(anim.Delay, delay)
-	}
-
-	_ = gif.EncodeAll(out, &anim)
 }
